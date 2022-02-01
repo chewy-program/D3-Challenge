3a87f54674247c20ce66e6e37cb2419bbb9b235c
Zachary Breen <zgbreen@gmail.com>
2022-02-01 15:29:41 +1100

    Update app.js

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,133 @@
 // @TODO: YOUR CODE HERE!
+function makeResponsive() {
+
+    // if the SVG area isn't empty when the browser loads,
+    // remove it and replace it with a resized version of the chart
+    var svgArea = d3.select("body").select("svg");
+
+    // clear svg is not empty
+    if (!svgArea.empty()) {
+        svgArea.remove();
+    }
+
+    // SVG wrapper dimensions are determined by the current width and
+    // height of the browser window.
+    var svgWidth = window.innerWidth;
+    var svgHeight = window.innerHeight;
+
+    var margin = {
+        top: 50,
+        bottom: 50,
+        right: 50,
+        left: 50
+    };
+
+    var height = svgHeight - margin.top - margin.bottom;
+    var width = svgWidth - margin.left - margin.right;
+
+    // Append SVG element
+    var svg = d3
+        .select(".chart")
+        .append("svg")
+        .attr("height", svgHeight)
+        .attr("width", svgWidth);
+
+    // Append group element
+    var chartGroup = svg.append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // Read CSV
+    d3.csv("assets/data/data.csv").then(function (demographic_data) {
+
+        // parse data
+        demographic_data.forEach(function (data) {
+            data.poverty = +data.poverty
+            data.healthcare = +data.healthcare;
+        });
+        // You need to create a scatter plot between two of the data variables such as `Healthcare vs. Poverty` or `Smokers vs. Age`.
+        // create scales
+        var xLinearScale = d3.scaleLinear()
+            .domain([20, d3.max(demographic_data, d => d.poverty)])
+            .range([0, width]);
+
+        var yLinearScale = d3.scaleLinear()
+            .domain([0, d3.max(demographic_data, d => d.healthcare)])
+            .range([height, 0]);
+
+        // create axes
+        var bottomAxis = d3.axisBottom(xLinearScale);
+        var leftAxis = d3.axisLeft(yLinearScale);
+
+        // append axes
+        chartGroup.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(bottomAxis);
+
+        chartGroup.append("g")
+            .call(leftAxis);
+
+        // line generator
+        var line = d3.line()
+            .x(d => xLinearScale(d.poverty))
+            .y(d => yLinearScale(d.healthcare));
+
+        // append line
+        chartGroup.append("path")
+            .data(demographic_data)
+            .attr("d", line)
+            .attr("fill", "none")
+            .attr("stroke", "red");
+
+        // append circles
+        var circlesGroup = chartGroup.selectAll("circle")
+            .data(demographic_data)
+            .enter()
+            .append("circle")
+            .attr("cx", d => xLinearScale(d.poverty))
+            .attr("cy", d => yLinearScale(d.healthcare))
+            .attr("r", "10")
+            .attr("fill", "gold")
+            .attr("stroke-width", "1")
+            .attr("stroke", "black");
+
+        // Step 1: Initialize Tooltip
+        var toolTip = d3.tip()
+            .attr("class", "tooltip")
+            .offset([80, -60])
+            .html(function (d) {
+                return (`${d.poverty}<br>Hair length: ${d.healthcare}<br>Hits: ${d.state}`);
+            });
+
+        // Step 2: Create the tooltip in chartGroup.
+        // chartGroup.call(toolTip);
+
+        // Step 3: Create "mouseover" event listener to display tooltip
+        circlesGroup.on("mouseover", function (d) {
+            toolTip.show(d, this);
+        })
+            // Step 4: Create "mouseout" event listener to hide tooltip
+            .on("mouseout", function (d) {
+                toolTip.hide(d);
+            });
+        chartGroup.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left + 40)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .attr("class", "axisText")
+            .text("% of poverty in population");
+
+        chartGroup.append("text")
+            .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+            .attr("class", "axisText")
+            .text("Hair Metal Band Hair Length (inches)");
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+// When the browser loads, makeResponsive() is called.
+makeResponsive();
+
+// When the browser window is resized, makeResponsive() is called.
+d3.select(window).on("resize", makeResponsive);
